a1ab9bb7c1700badd50140f7b81af6ec57022dcf
Dhirajdgandhi <ddgandhi.96@gmail.com>
2018-08-18 14:40:23 +0530

    BitBucket Webhook added

--- resources/scripts/server/refresh_and_restart.sh
+++ resources/scripts/server/refresh_and_restart.sh
@@ -1,7 +1,11 @@
+#!/bin/sh
 #Refresh code
+echo "Started Git Pull"
 expect git-pull-helper
 
 #Restart servers
 expect restart-gunicorn
 expect restart-nginx
 expect restart-mysql
+
+echo "Refresh and Restarts Done"
