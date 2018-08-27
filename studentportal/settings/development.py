8c7edf94deb9d4209af6286158caefdef28f8054
Dhirajdgandhi <ddgandhi.96@gmail.com>
2018-08-27 11:46:56 +0530

    Revert "Local changes 1"
    This reverts commit 83b3fc3

--- studentportal/settings/development.py
+++ studentportal/settings/development.py
@@ -9,7 +9,7 @@ DATABASES = {
     'default': {
        'ENGINE': 'django.db.backends.mysql',
         'NAME': 'portal_storage', #Make it testing later
-        'USER' : 'root',
+        'USER' : 'studentportal',
         'PASSWORD' : 'studentportal123',
         'HOST' : 'localhost',
         'PORT' : '3306',
@@ -18,6 +18,7 @@ DATABASES = {
 
 PROJECT_APPS=[
     'portalapp',
+    'clubsapp',
 ]
 
 INSTALLED_APPS = PREREQUISITE_APPS + PROJECT_APPS
